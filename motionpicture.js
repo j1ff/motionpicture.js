dfda530ec1cc79d3b39b30513982ef0d852080f4
j1ff <jlandfri@gmail.com>
2014-06-20 18:35:08 -0400

    got rid of extra defaults object fixes #8

--- motionpicture.js
+++ motionpicture.js
@@ -1,37 +1,35 @@
 (function($){
   $.fn.motionPicture = function(options){
-    var $window = $(window),
-      animations = []
-
-    var assembled = animationFactory(this, options);
-    animations.push(assembled);
-    return this;
-
+    animationFactory(this, options);
     // Make an object responsible for keeping track of the sprite status.
     function animationFactory($el, config) {
-      var defaults = {
-        startLogic: $(window).height(),
-        endLogic: $('html').height() - 1,
-        focalPoint: $(window).scrollTop() + $(window).height(),
-        loop: false,
-        hideOnComplete: true
-      };
-      var animationConfig = $.extend(defaults, config);
       var animObj = {
         self: this,
-        step: 0,
-        count: 0,
         el: $el,
-        baseClass: animationConfig.baseClass,
-        steps: animationConfig.steps,
+        startLogic: function() {
+          return $(window).height()
+        },
+        endLogic: function() {
+          return $('html').height() - 1;
+        },
+        focalPoint: function() {
+          return $(window).scrollTop() + $(window).height();
+        },
+        loop: false,
+        hideOnComplete: true,
+        step: 0,
+        steps: 0,
+        baseClass: 'motionpicture',
         hidden: false,
-        startLogic: animationConfig.startLogic,
-        endLogic: animationConfig.endLogic,
-        dynamicPlacement: animationConfig.dynamicPlacement,
+        dynamicPlacement: false,
         events: [{'event': 'scroll', 'target': $(window)}],
         currentClass: '',
-        hideOnComplete: animationConfig.hideOnComplete,
-        loop: animationConfig.loop,
+
+        onLoad: function() {
+          $.extend(this, config);
+          this.addEvents(this.el).calibrate();
+          return this;
+        },
 
         // Get the startpoint for the animation.
         getStart: function() {
@@ -59,7 +57,6 @@
           if (this.loop == true) {
             var loopFocal = (focalPoint - animStart) % animWidth,
             step = Math.round((loopFocal / animWidth) * ( this.steps));
-
             this.setAnimationStep(step);
           }
           else if ((animStart <= focalPoint) && (animEnd >= focalPoint)) {
@@ -78,23 +75,26 @@
           else {
             this.setAnimationStep(animEnd + 1);
           }
+          return this;
         },
 
         // Set this element to compute the sprite position any time that one of
         // the defined events triggers on one of the defined targets.
         addEvents: function(el) {
-          self = this;
+          var self = this;
           $.each(self.events, function() {
             $(this.target).on(this.event, function() {
               self.setSprite();
             });
           });
+          return this;
         },
 
         // Get relevant coordinates for calculating spriteStep.
         calibrate: function() {
           var $el = this.el;
           $el.data('animation-start', this.getStart()).data('animation-end', this.getEnd());
+          return this;
         },
 
         // Logic for manipulating dom so sprite is at the correct point.
@@ -111,12 +111,14 @@
               this.unhide();
             }
           }
+          return this;
         },
 
         // Helper method for adding a class.
         addClass: function(newClass) {
           this.el.addClass(newClass);
           this.currentClass = newClass;
+          return this;
         },
 
         // Helper function for hiding an element.
@@ -124,6 +126,7 @@
           this.el.hide();
           this.el.removeClass(this.currentClass);
           this.currentClass = '';
+          return this;
         },
 
         // Helper function for showing an element.
@@ -132,11 +135,7 @@
         }
       };
 
-
-      // Initialization.
-      animObj.calibrate();
-      animObj.addEvents(animObj.el);
-      return animObj;
+      return animObj.onLoad();
     }
   }
 })(jQuery);
